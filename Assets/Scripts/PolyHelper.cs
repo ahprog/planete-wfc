623be8d8a6b0425aca6b1c815cac9a2bf21d2af7
ahprog <herrero.antonin@gmail.com>
2020-05-02 22:13:42 +0200

    (feat) Ajout des faces sans la rotation

---
+++ Assets/Scripts/PolyHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolyHelper : MonoBehaviour
+{
+    public Transform facetPrefab;
+
+
+    public void BuildFaces()
+    {
+        Debug.Log("== ADDING FACES ==");
+
+        List<Transform> children = new List<Transform>();
+        foreach (Transform child in transform) {
+            children.Add(child);
+        }
+
+        foreach (Transform face in children) {
+            Debug.Log("CHILD PROCESSED : " + face.name);
+
+            Transform newFace = Instantiate(facetPrefab, transform);
+            newFace.localPosition = face.localPosition;
+            Vector3 normal = face.GetComponent<MeshFilter>().sharedMesh.normals[0];
+            newFace.forward = normal;
+        }
+    }
+}
---
+++ Assets/Scripts/PolyHelperEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PolyHelper))]
+public class PolyHelperEditor : Editor
+{
+    PolyHelper polyHelper;
+
+    private void OnEnable()
+    {
+        polyHelper = (PolyHelper)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Build faces")) {
+            polyHelper.BuildFaces();
+        }
+    }
+}
